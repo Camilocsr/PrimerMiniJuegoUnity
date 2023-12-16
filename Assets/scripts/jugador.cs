ad7adbb7d475c2e5e158a6003742a018d0624a20
Camilocsr <esmunred@gmail.com>
2023-12-16 18:33:14 -0500

    Primer abance

---
+++ Assets/scripts/jugador.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private new Rigidbody rigidbody; // Gravedad.
+    public float speed = 20f;
+    private float moveHorizontal, moveVertical,saltar; // Variables de nivel de clase
+    private bool enElSuelo = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>(); // Asigno la variable.
+        rigidbody.useGravity = true; // Activar gravedad después de asignar el componente Rigidbody.
+    }
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        if (enElSuelo && Input.GetKeyDown(KeyCode.Space))
+        {
+            saltar = 20f;
+            enElSuelo = false;
+            StartCoroutine(EsperarYCaer());
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, saltar, moveVertical * speed);
+
+        IEnumerator EsperarYCaer()
+        {
+            yield return new WaitForSeconds(1f); // Esperar 1 segundo
+            saltar = -20f; // Desactivar el salto para comenzar a caer
+            enElSuelo = true;
+        }
+
+        rigidbody.velocity = movement;
+    }
+}
